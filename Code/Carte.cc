7804bdc15aa4a940b82bb711b107ba6af2b5ae81
Emile Rey <erey@info-staxx.iut.bx1>
2013-02-06 13:26:47 +0100

    Correction chargement carte

--- Code/Carte.cc
+++ Code/Carte.cc
@@ -25,14 +25,14 @@ void Carte::chargementFichier(string mon_fichier)
 {
 	int tab[TAILLE_MAP][TAILLE_MAP];
 	int i=0, j=0, val=0;
-	
 	//initialisation
 	for (int i=0; i<TAILLE_MAP; i++)
 		for (int j=0; j<TAILLE_MAP; j++)
 			tab[i][j]=4;
 	
 	//Traitement (remplacer "maitreDeLaColline.txt par mon_fichier)
-	ifstream fichier(mon_fichier, ios::in);
+	
+	ifstream fichier("sdfsd.txt", ios::in);
 	if(fichier)
 	{
 		cout << "Ouverture de la map" << endl;
