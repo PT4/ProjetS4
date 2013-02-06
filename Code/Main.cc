f61dcbe6766b7bafc5df86f3329467c5c9a8afe1
Gaetan Roudeau <groudeau@info-sunx.iut.bx1>
2013-02-06 13:07:06 +0100

    maj code compile

--- Code/Main.cc
+++ Code/Main.cc
@@ -5,11 +5,8 @@
 #include "GameView.h"
 #include "Constantes.h"
 
-<<<<<<< HEAD
 #include "Soldat.h"
 #include "Entrepot.h"
-=======
->>>>>>> origin/master
 
 using namespace std;
 using namespace sf;
@@ -22,26 +19,14 @@ int main()
 	GameView * view = new GameView(LARGEUR_FENETRE, HAUTEUR_FENETRE);
 	view->setModel(model);
 	
-<<<<<<< HEAD
-	Soldat s1, s2;
-	
-	cout << " Avant attaque " << endl;
-	cout << "Soldat1 : " << "Vie " << s1.getPointsVie()  << " " << "Armure " << s1.getArmure() << " " << "Degat " << s1.getDegat() << endl;
-	cout << "Soldat2 : " << "Vie " << s2.getPointsVie() << endl;
-	
-	s1.attaquer(s2);
-	
-	cout << " Après attaque " << endl;
-	cout << "Soldat1 : " << "Vie " << s2.getPointsVie() << endl;
-	
 	while(view->treatEvents()){
 		/*if (!view->getMenu(view))
 			model->nextStep();*/
-=======
+	}
+
 	while(view->treatEvents())
 	{
 		model->nextStep();
->>>>>>> origin/master
 		view->draw();
 		usleep(11000);
 	}
--- Code/Unite.cc
+++ Code/Unite.cc
@@ -45,9 +45,6 @@ void Unite::seDeplacer() {
 }
 
 void Unite::attaquer(Entite& cible) {
-<<<<<<< HEAD
 	cible.setPointsVie(cible.getPointsVie()-m_degat);
-=======
 	cible.setPointsVie(getDegat());
->>>>>>> origin/master
 }
